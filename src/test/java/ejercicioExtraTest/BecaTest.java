d663feccddb669dcb57cb83c3fd7044ab2740ee4
Fabian Machicao <fabian.machicao@gmail.com>
2022-02-22 08:30:30 -0400

    commit 3

--- src/test/java/ejercicioExtraTest/BecaTest.java
+++ src/test/java/ejercicioExtraTest/BecaTest.java
@@ -13,30 +13,47 @@ import ejercicioExtra.Utils;
 public class BecaTest {
 	
 	Utils utilsMock = Mockito.mock(Utils.class);
-    MockedStatic<Helpers> helpersStatic= Mockito.mockStatic(Helpers.class);
+    
+	MockedStatic<Helpers> helpersStatic= Mockito.mockStatic(Helpers.class);
 
 
     @ParameterizedTest
-    @CsvSource({ "1111111,SI APLICA A BECA", 							// Si aplica beca
-          "2222222,NO APLICA A BECA POR PROMEDIO ACADEMICO",			//No aplica beca
-          "3333333,EL ESTUDIANTE NO CURSO AUN EL 60% DE LAS MATERIAS",	//Materias insufisientes
+    @CsvSource({ "100,SI APLICA A BECA", 							// Si aplica beca
+    	"101,SI APLICA A BECA",
+    	"102,SI APLICA A BECA",
+    	
+        "200,NO APLICA A BECA POR PROMEDIO ACADEMICO",			//No aplica beca
+        "201,NO APLICA A BECA POR PROMEDIO ACADEMICO",
+        "202,NO APLICA A BECA POR PROMEDIO ACADEMICO",
+        
+        
+        "300,EL ESTUDIANTE NO CURSO AUN EL 60% DE LAS MATERIAS",	//Materias insufisientes
+        "301,EL ESTUDIANTE NO CURSO AUN EL 60% DE LAS MATERIAS",
+        "302,EL ESTUDIANTE NO CURSO AUN EL 60% DE LAS MATERIAS",
     })
     public void recomendacionBecaTest(int ci, String expectedResult) {
-    	Mockito.when(utilsMock.getNota(1111111)).thenReturn(91);
     	
     	
-    	Mockito.when(utilsMock.getNota(2222222)).thenReturn(70);
+    	Mockito.when(utilsMock.getNota(100)).thenReturn(91);
+    	Mockito.when(utilsMock.getNota(101)).thenReturn(92);
+    	Mockito.when(utilsMock.getNota(102)).thenReturn(100);
     	
     	
-    	Mockito.when(utilsMock.getNota(3333333)).thenReturn(100);
+    	Mockito.when(utilsMock.getNota(200)).thenReturn(70);
+    	Mockito.when(utilsMock.getNota(201)).thenReturn(70);
+    	Mockito.when(utilsMock.getNota(202)).thenReturn(70);
     	
     	
+    	Mockito.when(utilsMock.getNota(300)).thenReturn(100);
+    	Mockito.when(utilsMock.getNota(301)).thenReturn(0);
+    	Mockito.when(utilsMock.getNota(302)).thenReturn(50);
     	
-    	helpersStatic.when(()-> Helpers.applicaBeca(1111111)).thenReturn(true);
     	
-    	helpersStatic.when(()-> Helpers.applicaBeca(2222222)).thenReturn(true);
+    	helpersStatic.when(()-> Helpers.applicaBeca(100)).thenReturn(true);
+    	
+    	helpersStatic.when(()-> Helpers.applicaBeca(200)).thenReturn(true);
 
-    	helpersStatic.when(()-> Helpers.applicaBeca(3333333)).thenReturn(false);
+    	helpersStatic.when(()-> Helpers.applicaBeca(300)).thenReturn(false);
     	
     	Beca beca = new Beca(utilsMock);
     	
@@ -44,7 +61,9 @@ public class BecaTest {
     	
         Assertions.assertEquals(expectedResult,actualResult,"ERROR el resultado es incorrecto");
 
-
+        
+        
+        helpersStatic.close();
     	
 
     }
